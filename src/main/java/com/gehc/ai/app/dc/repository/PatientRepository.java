7acd077947538807092ca24e6a05336c034004f2
Litao Yan <litao.yan@med.ge.com>
2016-11-02 22:50:51 -0500

    added rest end point per api spec

--- src/main/java/com/gehc/ai/app/dc/repository/PatientRepository.java
+++ src/main/java/com/gehc/ai/app/dc/repository/PatientRepository.java
@@ -1,7 +1,13 @@
 package com.gehc.ai.app.dc.repository;
 
 import com.gehc.ai.app.dc.entity.Patient;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
@@ -9,4 +15,12 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource(collectionResourceRel = "patient", path = "patient")
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+	List<Patient> findByPatientName(@Param("name") String name);
+	List<Patient> findByPatientId(@Param("id") String id);
+	List<Patient> findByGender(@Param("gender") String g);
+	List<Patient> findByAge(@Param("age") String id);
+	List<Patient> findByOrgId(@Param("orgid") String id);
+	
+	Page<Patient> findAllByOrderByIdDesc( Pageable pageable);
+
 }
