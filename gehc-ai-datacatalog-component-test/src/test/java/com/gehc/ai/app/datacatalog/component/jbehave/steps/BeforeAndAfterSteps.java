d0b9b955ddf665a962ad3a42286b9f961183cf34
Naidu, Sowjanya <sowjanya.naidu@ge.com>
2017-07-09 21:28:43 -0700

    US-3871:Adding Component Tests for DataCatalog data-set apis

--- gehc-ai-datacatalog-component-test/src/test/java/com/gehc/ai/app/datacatalog/component/jbehave/steps/BeforeAndAfterSteps.java
+++ gehc-ai-datacatalog-component-test/src/test/java/com/gehc/ai/app/datacatalog/component/jbehave/steps/BeforeAndAfterSteps.java
@@ -2,9 +2,11 @@ package com.gehc.ai.app.datacatalog.component.jbehave.steps;
 
 
 import com.gehc.ai.app.datacatalog.repository.AnnotationRepository;
+import com.gehc.ai.app.datacatalog.repository.DataSetRepository;
 import org.jbehave.core.annotations.BeforeScenario;
 import org.jbehave.core.annotations.ScenarioType;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import static org.mockito.Mockito.reset;
@@ -15,6 +17,9 @@ public class BeforeAndAfterSteps {
     @MockBean
     private AnnotationRepository annotationRepository;
 
+    @MockBean
+    DataSetRepository dataSetRepository;
+
     @BeforeScenario(uponType = ScenarioType.ANY)
     public void initStorageBeforeScenario() {
     }
@@ -22,6 +27,7 @@ public class BeforeAndAfterSteps {
     @BeforeScenario(uponType = ScenarioType.ANY)
     public void initMocksBeforeScenario() {
         reset(annotationRepository);
+        reset(dataSetRepository);
 
     }
 
