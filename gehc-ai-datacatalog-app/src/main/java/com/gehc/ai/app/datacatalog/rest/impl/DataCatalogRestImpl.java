e0a2cb6abded7048df0719d80f68937b93bd9777
212071558 <212071558@ge.com>
2017-07-13 17:15:41 -0700

    Added logic to check if patient DB id exists before calling patient details

--- gehc-ai-datacatalog-app/src/main/java/com/gehc/ai/app/datacatalog/rest/impl/DataCatalogRestImpl.java
+++ gehc-ai-datacatalog-app/src/main/java/com/gehc/ai/app/datacatalog/rest/impl/DataCatalogRestImpl.java
@@ -739,17 +739,16 @@ public class DataCatalogRestImpl implements IDataCatalogRest {
 
 	private List<ImageSeries> getPatientForImgSeriesLst(List<ImageSeries> imageSeriesLst) {
 		List<ImageSeries> imgSerWithPatientLst = new ArrayList<ImageSeries>();
-		try {
 			for (Iterator<ImageSeries> imgSeriesItr = imageSeriesLst.iterator(); imgSeriesItr.hasNext();) {
 				ImageSeries imageSeries = (ImageSeries) imgSeriesItr.next();
-				imageSeries.setPatient(patientRepository.findById(new Long(imageSeries.getPatientDbId())).get(0));
-//			logger.info("Get Patient Id in imageSeriesLst " + imageSeries.getPatient().getPatientId());
+				if(null != imageSeries && null != imageSeries.getPatientDbId()){
+					List<Patient> patientLst = patientRepository.findById(imageSeries.getPatientDbId());
+					if(null != patientLst && patientLst.size()>0){
+						imageSeries.setPatient(patientLst.get(0));
+					}
+				}
 				imgSerWithPatientLst.add(imageSeries);
 			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		return imgSerWithPatientLst;
 	}
 
