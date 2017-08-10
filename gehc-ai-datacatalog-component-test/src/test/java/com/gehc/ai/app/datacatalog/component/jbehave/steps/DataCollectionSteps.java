51c24e21d93b3559a473dfb27077ce1950d3c776
Naidu, Sowjanya <sowjanya.naidu@ge.com>
2017-08-10 15:22:49 -0700

    US-6341: Removing count attribute to imageset test valiadtions

--- gehc-ai-datacatalog-component-test/src/test/java/com/gehc/ai/app/datacatalog/component/jbehave/steps/DataCollectionSteps.java
+++ gehc-ai-datacatalog-component-test/src/test/java/com/gehc/ai/app/datacatalog/component/jbehave/steps/DataCollectionSteps.java
@@ -243,7 +243,7 @@ public class DataCollectionSteps {
     @Then("verify data collection image-set details by its id")
     public void thenVerifyDataCollectionImagesetDetailsByItsId() throws Exception {
         retrieveResult.andExpect(status().isOk());
-        retrieveResult.andExpect(content().string(containsString("[{\"id\":1,\"modality\":\"CT\",\"anatomy\":\"Lung\",\"dataFormat\":\"dataFormat\",\"uri\":\"tests3://gehc-data-repo-main/imaging/ct/lungData/LungCT_LIDC_LS/set10\",\"seriesInstanceUid\":\"1\",\"description\":\"test\",\"institution\":\"UCSF\",\"equipment\":\"tem\",\"instanceCount\":1,\"properties\":{\"test\":\"bdd\"},\"uploadBy\":\"BDD\",\"uploadDate\":\"2017-03-31\",\"patientDbId\":1,\"count\":0}]")));
+        retrieveResult.andExpect(content().string(containsString("[{\"id\":1,\"modality\":\"CT\",\"anatomy\":\"Lung\",\"dataFormat\":\"dataFormat\",\"uri\":\"tests3://gehc-data-repo-main/imaging/ct/lungData/LungCT_LIDC_LS/set10\",\"seriesInstanceUid\":\"1\",\"description\":\"test\",\"institution\":\"UCSF\",\"equipment\":\"tem\",\"instanceCount\":1,\"properties\":{\"test\":\"bdd\"},\"uploadBy\":\"BDD\",\"uploadDate\":\"2017-03-31\",\"patientDbId\":1}]")));
     }
 
     @Given("Post DataCatalog with Org ID null DataSetUp Provided")
