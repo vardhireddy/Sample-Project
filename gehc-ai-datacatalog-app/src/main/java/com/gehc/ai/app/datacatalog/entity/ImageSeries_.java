f8af8dc97ff9cbe26cd781b08e4166c129e2b766
Litao Yan <litao.yan@med.ge.com>
2017-08-25 11:34:49 -0700

    add some code to use jpa em for json queries

---
+++ gehc-ai-datacatalog-app/src/main/java/com/gehc/ai/app/datacatalog/entity/ImageSeries_.java
@@ -0,0 +1,14 @@
+package com.gehc.ai.app.datacatalog.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="gehc", date="2017-08-23")
+@StaticMetamodel(ImageSeries.class)
+public class ImageSeries_ {
+    public static volatile SingularAttribute<ImageSeries, Long> id;
+    public static volatile SingularAttribute<ImageSeries, String> uploadBy;
+    public static volatile SingularAttribute<ImageSeries, Integer> orgId;
+    public static volatile SingularAttribute<ImageSeries, String> uri;
+}
