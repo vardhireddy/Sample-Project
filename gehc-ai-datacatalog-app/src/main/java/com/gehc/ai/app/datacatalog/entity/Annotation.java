1685ebfbad407cf86409c90197325bdc735e0726
Tommy, Ambili <ambili.tommy@ge.com>
2018-02-21 12:53:58 -0800

    fixing as per review comments

--- gehc-ai-datacatalog-app/src/main/java/com/gehc/ai/app/datacatalog/entity/Annotation.java
+++ gehc-ai-datacatalog-app/src/main/java/com/gehc/ai/app/datacatalog/entity/Annotation.java
@@ -23,8 +23,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
-//import javax.validation.constraints.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
