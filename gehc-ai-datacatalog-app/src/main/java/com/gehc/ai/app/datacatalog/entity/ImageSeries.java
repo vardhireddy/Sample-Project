9407255dba09e17ce55fb2934f7db51dfbb65862
Dip Shah <dip.shah@ge.com>
2018-03-08 10:30:20 -0800

    Sonar Fixes

--- gehc-ai-datacatalog-app/src/main/java/com/gehc/ai/app/datacatalog/entity/ImageSeries.java
+++ gehc-ai-datacatalog-app/src/main/java/com/gehc/ai/app/datacatalog/entity/ImageSeries.java
@@ -272,7 +272,7 @@ public class ImageSeries implements Serializable {
 		this.uploadBy = uploadBy;
 	}
 	public Date getUploadDate() {
-		return this.uploadDate;
+		return (Date) this.uploadDate.clone();
 	}
 	public void setUploadDate(Date uploadDate) {
 		this.uploadDate = (Date) uploadDate.clone();
