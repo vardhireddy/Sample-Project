1f3b6f4f38d849ca9fb4351d4a1ed35671ee4f51
Dip Shah <dip.shah@ge.com>
2018-03-21 16:03:50 -0700

    Query Parameter Validation

---
+++ gehc-ai-datacatalog-app/src/test/java/com/gehc/ai/app/datacatalog/filters/RequestValidatorTest.java
@@ -0,0 +1,171 @@
+/**
+ * 
+ */
+package com.gehc.ai.app.datacatalog.filters;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.gehc.ai.app.datacatalog.exceptions.DataCatalogException;
+import com.gehc.ai.app.datacatalog.exceptions.ErrorCodes;
+
+/**
+ * @author dipshah
+ *
+ */
+public class RequestValidatorTest {
+	
+	Map<String, Object> paramMap = new HashMap<String, Object>();
+	
+	@Rule
+    public ExpectedException thrown = ExpectedException.none();
+	
+	@Before
+	public void init(){
+		paramMap.put("orgId", "159fa151-4cb9-4c78-8124-86be34d97954");
+		paramMap.put("dateFrom", "2017-12-14T19:00:00Z");
+		paramMap.put("dateTo", "2017-12-14T20:00:00Z");
+	}
+	
+	/**
+	 * Test method for {@link com.gehc.ai.app.datacatalog.filters.RequestValidator#validateImageSeriesFilterParamMap(java.util.Map)}.
+	 */
+	@Test
+	public void testValidateImageSeriesFilterParamMap(){
+		Map<String, Object> expectedMap = paramMap;
+		try {
+			RequestValidator.validateImageSeriesFilterParamMap(paramMap);
+			assertEquals(expectedMap, paramMap);
+		} catch (DataCatalogException e) {
+			fail();
+		}
+	}
+	
+	/**
+	 * Test method for {@link com.gehc.ai.app.datacatalog.filters.RequestValidator#validateImageSeriesFilterParamMap(java.util.Map)}.
+	 * @throws DataCatalogException 
+	 */
+	@Test
+	public void testValidateImageSeriesFilterParamMapNullDates() throws DataCatalogException{
+		paramMap.put("dateFrom", null);
+		paramMap.put("dateTo", null);
+		
+		thrown.expect(DataCatalogException.class);
+		thrown.expectMessage(ErrorCodes.MISSING_DATE_FROM_VALUE.getErrorMessage()+ErrorCodes.MISSING_DATE_TO_VALUE.getErrorMessage());
+		
+		RequestValidator.validateImageSeriesFilterParamMap(paramMap);
+	}
+	
+	/**
+	 * Test method for {@link com.gehc.ai.app.datacatalog.filters.RequestValidator#validateImageSeriesFilterParamMap(java.util.Map)}.
+	 * @throws DataCatalogException 
+	 */
+	@Test
+	public void testValidateImageSeriesFilterParamMapEmptyDates() throws DataCatalogException{
+		paramMap.put("dateFrom", "");
+		paramMap.put("dateTo", "");
+		
+		thrown.expect(DataCatalogException.class);
+		thrown.expectMessage(ErrorCodes.MISSING_DATE_FROM_VALUE.getErrorMessage()+ErrorCodes.MISSING_DATE_TO_VALUE.getErrorMessage());
+		
+		RequestValidator.validateImageSeriesFilterParamMap(paramMap);
+	}
+	
+	/**
+	 * Test method for {@link com.gehc.ai.app.datacatalog.filters.RequestValidator#validateImageSeriesFilterParamMap(java.util.Map)}.
+	 * @throws DataCatalogException 
+	 */
+	@Test
+	public void testValidateImageSeriesFilterParamMapInvalidObject() throws DataCatalogException{
+		paramMap.put("dateFrom", Integer.valueOf(1));
+		paramMap.put("dateTo", new Object());
+		
+		thrown.expect(DataCatalogException.class);
+		thrown.expectMessage(ErrorCodes.MISSING_DATE_FROM_VALUE.getErrorMessage()+ErrorCodes.MISSING_DATE_TO_VALUE.getErrorMessage());
+		
+		RequestValidator.validateImageSeriesFilterParamMap(paramMap);
+	}
+	
+	/**
+	 * Test method for {@link com.gehc.ai.app.datacatalog.filters.RequestValidator#validateImageSeriesFilterParamMap(java.util.Map)}.
+	 * @throws DataCatalogException 
+	 */
+	@Test
+	public void testValidateImageSeriesFilterParamMapInvalidDateFormat() throws DataCatalogException{
+		paramMap.put("dateFrom", "2017-12-1419:00:00Z");
+		paramMap.put("dateTo", "2017-12-14T20:00");
+		
+		thrown.expect(DataCatalogException.class);
+		thrown.expectMessage(ErrorCodes.INVALID_DATE_FROM_FORMAT.getErrorMessage()+ErrorCodes.INVALID_DATE_TO_FORMAT.getErrorMessage());
+		
+		RequestValidator.validateImageSeriesFilterParamMap(paramMap);
+	}
+	
+	/**
+	 * Test method for {@link com.gehc.ai.app.datacatalog.filters.RequestValidator#validateImageSeriesFilterParamMap(java.util.Map)}.
+	 * @throws DataCatalogException 
+	 */
+	@Test
+	public void testValidateImageSeriesFilterParamMapInvalidDateFromNullDateTo() throws DataCatalogException{
+		paramMap.put("dateFrom", "2017-12-1419:00:00Z");
+		paramMap.put("dateTo", null);
+
+		thrown.expect(DataCatalogException.class);
+		thrown.expectMessage(ErrorCodes.MISSING_DATE_TO_VALUE.getErrorMessage());
+		
+		RequestValidator.validateImageSeriesFilterParamMap(paramMap);
+	}
+	
+	/**
+	 * Test method for {@link com.gehc.ai.app.datacatalog.filters.RequestValidator#validateImageSeriesFilterParamMap(java.util.Map)}.
+	 * @throws DataCatalogException 
+	 */
+	@Test
+	public void testValidateImageSeriesFilterParamMapMissingDateFrom() throws DataCatalogException{
+		paramMap.remove("dateFrom");
+		
+		thrown.expect(DataCatalogException.class);
+		thrown.expectMessage(ErrorCodes.MISSING_DATE_FROM.getErrorMessage());
+		
+		RequestValidator.validateImageSeriesFilterParamMap(paramMap);
+	}
+	
+	/**
+	 * Test method for {@link com.gehc.ai.app.datacatalog.filters.RequestValidator#validateImageSeriesFilterParamMap(java.util.Map)}.
+	 * @throws DataCatalogException 
+	 */
+	@Test
+	public void testValidateImageSeriesFilterParamMapMissingDateTo() throws DataCatalogException{
+		paramMap.remove("dateTo");
+		
+		thrown.expect(DataCatalogException.class);
+		thrown.expectMessage(ErrorCodes.MISSING_DATE_TO.getErrorMessage());
+		
+		RequestValidator.validateImageSeriesFilterParamMap(paramMap);
+	}
+	
+	/**
+	 * Test method for {@link com.gehc.ai.app.datacatalog.filters.RequestValidator#validateImageSeriesFilterParamMap(java.util.Map)}.
+	 * @throws DataCatalogException 
+	 */
+	@Test
+	public void testValidateImageSeriesFilterParamMapToDateBeforeFromDate() throws DataCatalogException{
+		paramMap.put("dateFrom", "2017-12-14T20:00:00Z");
+		paramMap.put("dateTo", "2017-12-14T19:00:00Z");
+
+		thrown.expect(DataCatalogException.class);
+		thrown.expectMessage(ErrorCodes.DATE_FROM_AFTER_DATE_TO.getErrorMessage());
+		
+		RequestValidator.validateImageSeriesFilterParamMap(paramMap);
+	}
+	
+
+}
