fef4b4556f62d8f9e80a733868340aa7ea977540
Vikas Kadam <vikas.kadam1@ge.com>
2018-06-08 11:36:24 -0700

    Restrict swagger for datacatlog API documentation and remove generic ones

--- gehc-ai-datacatalog-app/src/main/java/com/gehc/ai/app/datacatalog/config/AppConfig.java
+++ gehc-ai-datacatalog-app/src/main/java/com/gehc/ai/app/datacatalog/config/AppConfig.java
@@ -23,7 +23,12 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.gehc.ai.app.interceptor.DataCatalogInterceptor;
+import com.google.common.base.Predicates;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -43,6 +48,15 @@ public class AppConfig extends WebMvcConfigurerAdapter{
     @Bean
     public DataCatalogInterceptor dataCatalogInterceptor() {
         return new DataCatalogInterceptor();
+    }
+    
+    @Bean
+    public Docket demoApi() {
+        return new Docket(DocumentationType.SWAGGER_2)//<3>
+                .select()//<4>
+                .apis(RequestHandlerSelectors.basePackage("com.gehc.ai.app.datacatalog"))//<5>
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
     }
     
     @Override
