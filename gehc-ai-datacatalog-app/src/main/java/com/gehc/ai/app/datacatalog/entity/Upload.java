9e8bbe119194957aca017b008246f00abbccf505
Aruna Sindhura <arunasindhu.gorantla@ge.com>
2018-07-14 11:36:27 -0700

    PUT Upload API

--- gehc-ai-datacatalog-app/src/main/java/com/gehc/ai/app/datacatalog/entity/Upload.java
+++ gehc-ai-datacatalog-app/src/main/java/com/gehc/ai/app/datacatalog/entity/Upload.java
@@ -22,7 +22,13 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
+import javax.persistence.Convert;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Version;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.sql.Timestamp;
